8687c82e46d3d346b58f77289bccb0dd4c073606
mayhim182 <mayankatnitrkl@gmail.com>
2024-02-22 09:47:22 +0530

    props

--- egghead/src/App.js
+++ egghead/src/App.js
@@ -1,5 +1,19 @@
 import React from "react";
 
-const App = ()=> <h1>Namaste</h1>
+class App extends React.Component{
+  render(){
+    return (
+      <div>
+         <h1>{this.props.txt}</h1>
+          <b>Google</b>
+      </div>
+    );
+  }
+}
+
+App.propTypes = {
+  txt:React.propTypes.string,
+  cat:React.propTypes.number
+}
 
 export default App;
