a44e1a52661e80ee669550fc39736944f2912a97
mayhim182 <mayankatnitrkl@gmail.com>
2024-02-23 09:53:51 +0530

    daily code push

--- egghead/src/App.js
+++ egghead/src/App.js
@@ -4,12 +4,14 @@ class App extends React.Component{
   constructor(){
     super();
     this.state = {
-      txt:'this is the state txt'
+      txt:'this is the state txt',
+      currentEvent:'---' //This one is for synthetic event system
     }
+    this.update = this.update.bind(this)
   }
 
   update(e){
-    this.setState({txt:e.target.value});
+    this.setState({txt:e.target.value,currentEvent:e.type});
   }
 
   render(){
@@ -18,9 +20,22 @@ class App extends React.Component{
         <Widget update={this.update.bind(this)} />
         <h1>{this.props.txt}</h1>
         <h2>{this.state.txt}</h2>
-        <Button>React</Button>
+        <Button>I <Heart /> React</Button>
+        <Title text="GRAND Gesture"/>
+        <textarea
+          onKeyPress={this.update}
+          onCopy={this.update}
+          onPaste={this.update}
+           cols="30" rows="10" />
+          <h1>{this.state.currentEvent}</h1>
       </div>
     );
+  }
+}
+
+class Heart extends React.Component{
+  render(){
+    return <span>&hearts;</span>
   }
 }
 
@@ -40,5 +55,21 @@ const Widget = (props) =>{
 const Button = (props) =>{
   return <button>{props.children}</button>
 }
+
+const Title = (props) => <h1>Title:{props.text}</h1>
+
+Title.propTypes = {
+  text(props, propName, component){
+    if(!(propName in props)){
+      return new Error(`missing ${propName}`)
+    }
+    if(props[propName].length < 6){
+      return new Error(`${propName} is too short`)
+    }
+  }
+}
+
+//Normalise events with reacts synthetic event system
+
 
 export default App;
